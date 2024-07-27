493e78da31af64c3a8fea45a308c2f5d1a550649
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-07-27 21:49:10 +0200

    first movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed;
-    public float jumpForce;
+    public float moveSpeed = 5f;
+    public float jumpForce = 20f;
+
+    private Vector3 moveDirection;
 
     void Start()
     {
@@ -16,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        transform.position += moveDirection * Time.deltaTime * moveSpeed;
     }
 }
