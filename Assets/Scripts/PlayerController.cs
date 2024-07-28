1ea762acbdd9c9865ae952d9afd32d5ee0aea7c5
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-07-28 21:02:04 +0200

    player rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,11 @@ public class PlayerController : MonoBehaviour
 {
 
     public CharacterController charCon;
-    
+    public GameObject playerModel;
     
     public float moveSpeed = 5f;
     public float jumpForce = 15f;
+    public float rotateSpeed = 5f;
 
     private Vector3 moveDirection;
     private Camera camera;
@@ -27,7 +28,8 @@ public class PlayerController : MonoBehaviour
     {
         float yStore = moveDirection.y;
 
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
         moveDirection *= moveSpeed;
 
         moveDirection.y = yStore;
@@ -45,6 +47,8 @@ public class PlayerController : MonoBehaviour
         if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             transform.rotation = Quaternion.Euler(0f, camera.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
         }
 
         
