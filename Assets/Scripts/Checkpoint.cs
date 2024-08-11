6137f15c88c0000abe86ecffff6be757169ad8ac
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-11 07:20:13 +0200

    checkpoint improve

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    public GameObject cpActive;
+    public GameObject cpInactive;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             GameManager.instance.SetCheckpoint(transform.position);
+            ActivateCheckpoint();
         }
+    }
+
+    public void ActivateCheckpoint()
+    {
+        cpActive.SetActive(true);
+        cpInactive.SetActive(false);
+    }
+
+    public void DeactivateCheckpoint()
+    {
+        cpActive.SetActive(false);
+        cpInactive.SetActive(true);
     }
 }
